33973de58089bf6dc3615a35804307ce371920b6
Bryan Gentz <BryanGentz88@gmail.com>
2020-04-06 08:24:56 -0400

    Initial Frontend Commit

--- trending-neighborhood/src/App.js
+++ trending-neighborhood/src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+          <div>
+            <NavBar />
+            <hr />
+            <Route exact path="/" component={HomePage} />
+            {/* <Route exact path="/articles/:articleID" component={ArticlePage} />
+            <Route exact path="/sections/:sectionID" component={SectionPage} />
+            <Route exact path="/add-article" component={AddArticlePage} />
+            <Route exact path="/login" component={LoginPage} /> */}
+          </div>
+        </Router>
     </div>
   );
 }
