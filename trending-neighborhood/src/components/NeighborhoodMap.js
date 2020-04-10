4cccaf2547f43769bc65ed149fa18b81c1f59027
kenmalley <kennethmalley@hotmail.com>
2020-04-10 17:12:56 -0500

    changes

--- trending-neighborhood/src/components/NeighborhoodMap.js
+++ trending-neighborhood/src/components/NeighborhoodMap.js
@@ -3,7 +3,11 @@ import '../App.css'
 import { Map, GeoJSON, Marker, Popup, TileLayer } from 'react-leaflet'
 import ChicagoNeighborhoods from '../geojson/chicago-zillow-neighborhoods.json';
 
+
+
+
 class NeighborhoodMap extends Component{
+  
   state = {
     lat: 41.878,
     lng: -87.629,
@@ -64,26 +68,65 @@ class NeighborhoodMap extends Component{
     }
   }
 
-  geoJSONStyle() {
-    return {
+  geoJSONStyle(feature) {
+    console.log("I'm the feature!",feature)
+    console.log("I'm the props!", this.props)
+
+    return {      
       color: '#1f2021',
       weight: 1,
       fillOpacity: 0.25,
       fillColor: '#fff2af',
+
     }
   }
 
-  onEachFeature(feature: Object, layer: Object) {
+  onEachFeature(feature, layer)  {
+    // console.log("I'm the props!",props)
+
     const popupContent = ` <Popup><p>Customizable Popups <br />with feature information.</p><pre>Neighborhood: <br />${feature.properties.name}</pre></Popup>`
     layer.bindPopup(popupContent)
+    // let neighborhood = this.props.results.filter(el => el["Neighborhood"] == feature.properties.name)
+    // console.log(neighborhood)
+    // layer.setStyle({
+    //   color: '#1f2021',
+    //   weight: 1,
+    //   fillOpacity: 0.25,
+    //   fillColor: '#fff2af',
+    // })
   }
 
 
+  getScoreColor = (geoJsoNneighborhood) => {
+    let neighborhood = this.props.results.find(el => el["Overall Score"] == geoJsoNneighborhood)
+    console.log(neighborhood)
+    // let score = neighborhood["Overall Score"]
+    // switch(true) {
+    //   case (score >= 90):
+    //     return '#006d2c'
+    //   case (score >= 80):
+    //     return '#238b45'
+    //   case (score >= 70):
+    //     return '#41ae76'
+    //   case (score >= 60):
+    //     return '#66c2a4'
+    //   case (score >= 50):
+    //     return '#99d8c9'
+    //   case (score >= 40):
+    //     return '#ccece6'
+    //   case (score >= 30):
+    //     return '#e5f5f9'
+    //   default:
+    //     return '#f7fcfd'
+    // }
+  }
+
   
   render() {
-    
-    // console.log(this.props)
+    console.log(this.props)
     const position = [this.state.lat, this.state.lng]
+    const props = this.props.results
+    // console.log("Props", props)
     return (
       <Map scrollWheelZoom={false} center={position} zoom={this.state.zoom} style={{height: "960px", width: '100%'}}>
         <TileLayer
@@ -92,8 +135,10 @@ class NeighborhoodMap extends Component{
         />
         <GeoJSON
           data={ChicagoNeighborhoods}
-          style={this.geoJSONStyle}
+          style= {this.geoJSONStyle}
+
           onEachFeature={this.onEachFeature}
+     
         />
       </Map>
     );
--- trending-neighborhood/src/components/ScoreBreakdown.js
+++ trending-neighborhood/src/components/ScoreBreakdown.js
@@ -5,19 +5,27 @@ import ReactSpeedometer from "react-d3-speedometer"
 class ScoreBreakdown extends Component {
 
   render() {
+    let sortedArray=[]
+    for (let i in this.props.userPreferences){sortedArray.push([this.props.userPreferences[i], i])}
+    sortedArray=sortedArray.reverse().sort().reverse() 
+
+   
+        
     return (
-      <div>
-        <CardDeck className="row row-cols-1 row-cols-lg-2  row-cols-xl-3"> 
-        {Object.entries(this.props.categories).map((category) => {
+      <div >
+        <h1>Showing neighborhood: {this.props.results["Neighborhood"]} </h1>
+        <CardDeck className="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3  row-cols-xl-4"> 
+
+        {sortedArray.map(category => {
           return (
             <Col className="my-4">
-              <Card >
-              <ReactSpeedometer
+              <Card style={{width: '250px'}}>
+              {/* <ReactSpeedometer
                 width={300}
                 height={175}
                 needleHeightRatio={0.7}
                 // Get value from API Return not the category score
-                value={category[1] * 10} 
+                value={this.props.results.breakdown[category[1]] * 10} 
                 currentValueText="Happiness Level"
                 customSegmentLabels={[
                   {
@@ -52,12 +60,31 @@ class ScoreBreakdown extends Component {
                 needleTransition="easeElastic"
                 needleColor={"#90f2ff"}
                 textColor={"#d8dee9"}
-              />
+              /> */}
+              
             <Card.Body>
-              <Card.Title>{category[0]}</Card.Title>
+              {/* {(()=>{
+              let score = parseInt(this.props.results.breakdown[category[1]])
+              let color="black"
+
+                switch(true) {
+                  case score>80:
+                    color="green"
+                    break
+                  case score>60:
+                    color="yellow"
+                    break
+                  default:
+                    color="red"
+                    break}
+
+                  })} */}
+                  <h1 style={{fontSize: '100px', color: 'green'}}>{this.props.results.breakdown[category[1]]}</h1>
+              <Card.Title>{category[1]}</Card.Title>
             </Card.Body>
             <Card.Footer>
-              <small className="text-muted">Maybe a link to {category[0]} in x neighborhood</small>
+              <small className="text-muted">Maybe a link to {category[1]} in {this.props.results["Neighborhood"]}</small>
+              
             </Card.Footer>
           </Card>
             </Col>
